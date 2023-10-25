2d044281849feacd600e0e0566e0192c301c3871
Daniel Marques <76397634+danielmaques@users.noreply.github.com>
2023-10-25 16:03:44 -0300

    feat: add delete transaction

--- handler/deleteTransaction.go
+++ handler/deleteTransaction.go
@@ -1,13 +1,32 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/danielmaques/finance_api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteTransactionHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"message": "DELETE /api/v1/transactions",
-	})
+	id := context.Query("id")
+
+	if id == "" {
+		sendError(context, http.StatusBadRequest, errParamsRequired("id", "string").Error())
+		return
+	}
+
+	transaction := schemas.Transaction{}
+
+	if err := db.First(&transaction, id).Error; err != nil {
+		sendError(context, http.StatusNotFound, fmt.Sprintf("transaction with id %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&transaction).Error; err != nil {
+		sendError(context, http.StatusInternalServerError, fmt.Sprintf("error deleting transaction with id %s", id))
+		return
+	}
+
+	sendSuccess(context, "delete transaction", transaction)
 }
