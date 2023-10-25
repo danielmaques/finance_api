18964992c05362e02c8efc12a8a95b9ba768bad6
Daniel Marques <76397634+danielmaques@users.noreply.github.com>
2023-10-25 16:46:43 -0300

    feat: swagger logic

--- router/routers.go
+++ router/routers.go
@@ -1,12 +1,18 @@
 package router
 
 import (
+	"github.com/danielmaques/finance_api/docs"
 	"github.com/danielmaques/finance_api/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	handler.InitializeHandler()
+
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
 
 	v1 := router.Group("/api/v1")
 	{
@@ -15,4 +21,7 @@ func initializeRoutes(router *gin.Engine) {
 		v1.DELETE("/transactions", handler.DeleteTransactionHandler)
 		v1.PUT("/transactions", handler.UpdateTransactionHandler)
 	}
+
+	// Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
